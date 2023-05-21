797b00521de6f68c6cf8c3c64d228568c7e8788f
Jack Sanders <jacklewissanders@gmail.com>
2023-05-21 12:54:32 -0500

    modified:   .env 	modified:   src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,6 @@ async function bootstrap() {
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();
