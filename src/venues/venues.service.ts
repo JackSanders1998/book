36d4f48c2abad9c3da8427f00d0fc8a13c051b52
Jack Sanders <jacklewissanders@gmail.com>
2023-05-29 17:43:01 -0500

    logging and error handling experimentation

--- src/venues/venues.service.ts
+++ src/venues/venues.service.ts
@@ -1,55 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { Prisma, Venue } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateVenueDto } from './dto/create-venue.dto';
+import { isEmpty } from 'lodash';
 
 @Injectable()
 export class VenuesService {
   constructor(private prisma: PrismaService) {}
+  private readonly venuesLogger = new Logger(VenuesService.name);
 
-  create(data: CreateVenueDto): Promise<Venue> {
-    return this.prisma.venue.create({
+  async create(data: CreateVenueDto): Promise<Venue> {
+    this.venuesLogger.log('[create] input', data);
+    const venue = await this.prisma.venue.create({
       data,
     });
+    this.venuesLogger.log(`[create] Created venue with id: ${venue.id}`);
+    return venue;
   }
 
-  findAll(params: {
+  async findAll(params: {
     skip?: number;
     take?: number;
     cursor?: Prisma.VenueWhereUniqueInput;
     where?: Prisma.VenueWhereInput;
     orderBy?: Prisma.VenueOrderByWithRelationInput;
   }): Promise<Venue[]> {
-    const { skip, take, cursor, where, orderBy } = params;
-    return this.prisma.venue.findMany({
-      skip,
-      take,
-      cursor,
-      where,
-      orderBy,
-    });
+    this.venuesLogger.log('[findAll] input', params);
+    const venues = await this.prisma.venue.findMany(params);
+    this.venuesLogger.log(`[findAll] Found ${venues.length} venues`);
+    return venues;
   }
 
-  findOne(where: Prisma.VenueWhereUniqueInput): Promise<Venue | null> {
-    return this.prisma.venue.findUnique({
+  async findOne(where: Prisma.VenueWhereUniqueInput): Promise<Venue | null> {
+    this.venuesLogger.log('[findOne] input', where);
+    const venue = await this.prisma.venue.findUnique({
       where,
     });
+    if (isEmpty(venue)) {
+      this.venuesLogger.warn(`[findOne] No venue found with id: ${where.id}`);
+      throw new NotFoundException(`No venue found with id: ${where.id}`);
+    }
+    this.venuesLogger.log(`[findOne] Venue found with id: ${where.id}`, venue);
+    return venue;
   }
 
-  update(params: {
+  async update(params: {
     where: Prisma.VenueWhereUniqueInput;
     data: Prisma.VenueUpdateInput;
   }): Promise<Venue> {
-    const { where, data } = params;
-    return this.prisma.venue.update({
-      data,
-      where,
-    });
+    this.venuesLogger.log('[update] input', params);
+    await this.findOne(params.where);
+    const venue = await this.prisma.venue.update(params);
+    this.venuesLogger.log(
+      `[update] Venue updated with id: ${params.where.id}`,
+      venue,
+    );
+    return venue;
   }
 
-  remove(where: Prisma.VenueWhereUniqueInput): Promise<Venue> {
-    return this.prisma.venue.delete({
+  async remove(where: Prisma.VenueWhereUniqueInput): Promise<Venue> {
+    this.venuesLogger.log('[remove] input', where);
+    await this.findOne(where);
+    const venue = this.prisma.venue.delete({
       where,
     });
+    this.venuesLogger.log(`[delete] Venue updated with id: ${where.id}`, venue);
+    return venue;
   }
 }
